bb517e075a42599550991c4290eaf24dd79e6338
reaganwei0216 <reaganwei0216@gmail.com>
2018-08-18 15:50:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text="hello"))
+        TextSendMessage("hello"))
  
 
 if __name__ == '__main__':
